d42eaca7d2cce14543f21f0ec568f31309657421
jefree tamayo <jefree.tamayo@est.iudigital.edu.co>
2025-05-25 12:35:00 -0500

    creamos el metodo para capturar las 5 personas con sus datos

--- src/ListaPersonas.java
+++ src/ListaPersonas.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ListaPersonas {
     private static class persona {
@@ -14,9 +15,42 @@ public class ListaPersonas {
             this.genero = genero;
             this.edad = edad;
         }
+        @Override
+        public String toString(){
+            return "nombre: " + nombre + "/" +
+                    " apellido: " + apellido + "/" +
+                    " genero: " + genero + "/" +
+                    " edad: " + edad;
+        }
+    }
+    public static ArrayList<persona> listaDePersonas () {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<persona> listaDatos = new ArrayList<>();
+        int contadorPersonas = 0;
+        int numeroPersonas = 5;
+        int sumaEdades = 0;
+
+        while (contadorPersonas < numeroPersonas) {
+            System.out.println("ingrese los datos de la persona #" + (contadorPersonas + 1) + ":");
+
+            System.out.println("ingrese el nombre:");
+            String nombre = scanner.nextLine();
+
+            System.out.println("ingrese el apellido:");
+            String apellido = scanner.nextLine();
+
+            System.out.println("ingrese el genero:");
+            String genero = scanner.nextLine();
+
+            System.out.println("ingrese la edad:");
+            int edad = scanner.nextInt();
+            scanner.nextLine();
+            sumaEdades += edad;
+
+            persona nuevaPersona = new persona(nombre, apellido, genero, edad);
+        }
+        scanner.close();
+        return listaDatos;
     }
 
 }
-
-
-
