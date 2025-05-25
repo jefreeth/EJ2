d6faa6b38ccb917ddd28413fe9581948e0eeba44
jefree tamayo <jefree.tamayo@est.iudigital.edu.co>
2025-05-25 12:22:41 -0500

    Cremao las clase persona y el constructor con atributos privados

--- src/ListaPersonas.java
+++ src/ListaPersonas.java
@@ -2,4 +2,21 @@ import java.util.ArrayList;
 import java.util.ArrayList;
 
 public class ListaPersonas {
+    private static class persona {
+        private String nombre;
+        private String apellido;
+        private String genero;
+        private int edad;
+
+        public persona(String nombre, String apellido, String genero, int edad) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.genero = genero;
+            this.edad = edad;
+        }
+    }
+
 }
+
+
+
